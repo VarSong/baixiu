1a40a03cd46e1da34a22862fbe272dbcb2f2e5a6
watermeter <checkthewatermeter@outlook.com>
2018-11-11 12:57:34 +0800

    object

---
+++ admin/login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+  <meta charset="utf-8">
+  <title>Sign in &laquo; Admin</title>
+  <link rel="stylesheet" href="/static/assets/vendors/bootstrap/css/bootstrap.css">
+  <link rel="stylesheet" href="/static/assets/vendors/animate/animate.css">
+  <link rel="stylesheet" href="/static/assets/css/admin.css">
+</head>
+<body>
+  <div class="login">
+    <!-- 可以通过在 form 上添加 novalidate 取消浏览器自带的校验功能 -->
+    <!-- autocomplete="off" 关闭客户端的自动完成功能 -->
+    <form class="login-wrap" method="post" autocomplete="off" novalidate>
+      <img class="avatar" src="/static/assets/img/default.png">
+      <!-- 作为一个优秀的页面开发人员，必须考虑一个页面的不同状态下展示的内容不一样的情况 -->
+      <!-- 有错误信息时展示 -->
+      <div class="alert alert-danger">
+        <strong>错误！</strong>
+      </div>
+      <div class="form-group">
+        <label for="email" class="sr-only">邮箱</label>
+        <input id="email" name="email" type="email" class="form-control" placeholder="邮箱" autofocus value="">
+      </div>
+      <div class="form-group">
+        <label for="password" class="sr-only">密码</label>
+        <input id="password" name="password" type="password" class="form-control" placeholder="密码">
+      </div>
+      <button class="btn btn-primary btn-block">登 录</button>
+    </form>
+  </div>
+</body>
+</html>
